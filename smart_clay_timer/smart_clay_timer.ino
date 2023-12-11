39a4e56365df666f91d695c040a442de2e55eb32
unknown <60006290+ethmacc@users.noreply.github.com>
2023-12-11 23:37:25 +0000

    Fix map function with constrain in arduino code

--- smart_clay_timer/smart_clay_timer.ino
+++ smart_clay_timer/smart_clay_timer.ino
@@ -9,21 +9,24 @@
 #define DHTTYPE DHT22
 
 //Define pins
-#define LEDPin 12
+#define LEDPin 14
 #define trigPin 15
 #define echoPin 13
 #define DHTPin 2
 
 // DHT Sensor setup
 DHT dht(DHTPin, DHTTYPE); // Initialize DHT sensor
+float temp;
+float hum;
 
 //Neopixel setup
 #define NUMPIXELS 8
 Adafruit_NeoPixel pixels(NUMPIXELS, LEDPin);
 uint32_t yellow = pixels.Color(20, 20, 0);
 uint32_t green = pixels.Color(0, 40, 0);
-uint32_t red = pixels.Color(40, 0, 0);
 uint32_t orange = pixels.Color(30, 10, 0);
+uint32_t blue = pixels.Color(0, 0, 40);
+uint32_t colour;
 
 //Thermal camera setup
 Adafruit_MLX90640 mlx; 
@@ -48,6 +51,8 @@ void setup() {
   // start DHT sensor
   pinMode(DHTPin, INPUT);
   dht.begin();
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
   
   //Thermal camera settings
   mlx.setMode(MLX90640_CHESS);
@@ -81,7 +86,7 @@ void loop() {
 
   // Conditional, only start (and continue) taking readings if an object is in front for longer than 5s
   if (distance <= 15 && !status){
-    pixels.fill(yellow, 0, 8);
+    pixels.fill(green, 0, 8);
     pixels.show();
     delay(250);
     pixels.clear();
@@ -121,41 +126,43 @@ void readTemp() {
       mode_count = std::max(mode_count, histogram[T]);
     }
   }
-  // Retrieve the mode(s) 
-  for (auto T: histogram){
-    if (T.second == mode_count) {
-      int i = map(T.first, 15, 30, 0, 8);
 
-      if (i >= 0 && i < 3){
-        pixels.fill(red, 0, i);
-        pixels.show();
-      }
-
-      else if (i >= 3 && i < 5){
-        pixels.fill(orange, 0, i);
-        pixels.show();
-      }
-
-      else if (i >= 5 && i < 7){
-        pixels.fill(yellow, 0, i);
-        pixels.show();
-      }
-
-      else {
-        pixels.fill(green, 0, i);
-        pixels.show();
-      }
-
-      Serial.print("The mode temperature is: ");
-      Serial.println(T.first);      
-    }
-  }
   t_now2 = millis();
   if (t_now2 - t_last2 > 20000) {
+    temp = dht.readTemperature();
+    hum = dht.readHumidity(); 
     Serial.print("Temp: ");
     Serial.println(dht.readTemperature());
     Serial.print("Hum: ");
     Serial.println(dht.readHumidity());
     t_last2 = millis();
+  }
+
+  if (hum < 20){
+    colour = orange;
+  }
+
+  else if (hum >= 20 && hum < 40){
+    colour = yellow;
+  }
+
+  else if (hum >= 40) {
+    colour = blue;
+  }
+  
+    // Retrieve the mode(s) 
+  for (auto T: histogram){
+    if (T.second == mode_count) {
+      int T_constrain = constrain(T.first, 18, 24);
+      int i = map(T_constrain, 18, 24, 1, 8);
+      Serial.println(i);
+      Serial.println(T_constrain);
+
+      pixels.fill(colour, 0, i);
+      pixels.show();
+
+      Serial.print("The mode temperature is: ");
+      Serial.println(T.first);      
+    }
   }
 }
